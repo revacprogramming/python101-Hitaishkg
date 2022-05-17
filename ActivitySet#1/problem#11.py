ba8d673cabb35b072b21286411bb22ced7e61f60
hitaish kg <91547035+Hitaishkg@users.noreply.github.com>
2022-05-17 17:17:23 +0000

    p11,p12 done

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,12 +1,16 @@
 fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+count = 0
 fh = open(fname)
-lst = list()
-for line in fh:
-    t=line.rstrip().split()
-    for element in t:
-     if element not in lst:
-      lst.append(element)
-    else:
-        continue
-lst.sort() 
-print(lst)
+for k in fh:
+    if k.startswith('From:'):
+            continue
+    if  k.startswith('From'):               
+     	word=k.split()
+     	print(word[1])
+     	count=count+1
+            
+
+print("There were", count, "lines in the file with From as the first word")
--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,23 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+d=dict()
+big=int(0)
+for line in handle:
+    words=line.split()
+    if len(words) < 3 or words[0] != "From" : continue
+    words=words[1]
+    if words not in d:
+        d[words]=1
+    else :
+        d[words]+=1
+    d[words]=int(d[words])
+    
+maxx=0
+value=""
+for i in d:
+    if (d[i])>maxx:
+        maxx=d[i]
+        value=i
+print(value,maxx)
