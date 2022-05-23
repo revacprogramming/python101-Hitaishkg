c649bf5de8f3977a0057b458117b891d2ef7eb4d
hitaish kg <91547035+Hitaishkg@users.noreply.github.com>
2022-05-23 09:29:04 +0000

    13 done

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,22 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+import urllib.request, urllib.parse, urllib.error
+
+ img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg').read()
+ fhand = open('cover3.jpg', 'wb')
+ fhand.write(img)
+ fhand.close()
+
+ import socket
+
+ mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ mysock.connect(('data.pr4e.org', 80))
+ cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+ mysock.send(cmd)
+
+ while True:    data = mysock.recv(512)
+     if len(data) < 1:
+         break
+     print(data.decode(),end='')
+
+ mysock.close()
