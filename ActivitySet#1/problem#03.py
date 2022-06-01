da69f7208bec7bae2e5e64a762aaf112af3e30d0
hitaish kg <91547035+Hitaishkg@users.noreply.github.com>
2022-06-01 04:23:35 +0000

    Activity set1 problem 3 modified

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,6 +1,21 @@
 # Variables, Expressions & Statements
 
-hrs = input("Enter Hours:")
-rate_perhr=input("Enter rate per hour")
-gross_pay= (float(hrs) * float(rate_perhr))
-print("Pay: "+str(gross_pay))
+def inpuy():
+    s = input("Enter values:")
+    return s
+
+def compute(hrs,rate_perhr):
+    gross_pay= (float(hrs) * float(rate_perhr))
+    return gross_pay
+    
+def main():
+    hrs=inpuy()
+    rate_perhr=inpuy()
+    gross_pay=compute(hrs,rate_perhr)
+    print(gross_pay)
+
+
+if __name__=="__main__":
+    main()
+    
+
