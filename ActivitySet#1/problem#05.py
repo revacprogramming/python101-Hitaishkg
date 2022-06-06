57d73151ecd129e57688f547dc4fac1ad5ae30ef
hitaish kg <91547035+Hitaishkg@users.noreply.github.com>
2022-06-06 08:24:00 +0000

    ..

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,15 +1,30 @@
-score = input("Enter Score: ")
-score=float(score)
-if score>1.0 or score<0.1:
-    print("Marks entered out of range")
-    quit()
-elif score>=0.9:
-    print("A")
-elif score>=0.8:
-    print("B")
-elif score>=0.7:
-    print("c")
-elif score>=0.6:
-    print("D")
-elif score<0.6:
-    print("F")
+def inpuy():
+    s=float(input('Enter values'))
+    return s 
+
+def compute(score):
+    if score>1.0 or score<0.1:
+     print("Marks entered out of range")
+     quit()
+    elif score>=0.9:
+     grade="A"
+    elif score>=0.8:
+        grade="B"
+    elif score>=0.7:
+     grade="C"
+    elif score>=0.6:
+     grade="D"
+    elif score<0.6:
+     grade ="F"
+    return grade
+
+def output(grade):
+    print(grade)
+
+def main():
+    score=inpuy()
+    grade=compute(score)
+    output(grade)
+
+if __name__=="__main__":
+    main()
