fcc2bd0e24e07d6b5a2799f5bb59362910c7e2c2
hitaish kg <91547035+Hitaishkg@users.noreply.github.com>
2022-06-12 05:17:55 +0000

    ActivitySet2 p5 Done

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -2,14 +2,28 @@
 
 def get_cs():
     """get string input"""
+    s = input("Enter string: ")
+    return s
+
 
 
 def cs_to_dict(cs):
     """convert connect string to a dictionary"""
+    dic={}
+    cs=cs.split(";")
+    for i in cs:
+        i = i.split("=")
+        dic[i[0]]=i[1]
+    return dic
 
 
 def dict_to_cs(d):
     """convert a dictionary to connect string"""
+    l=' '
+    for i in d:
+        l+= i+"="+d.get(i)+";"
+
+    return l
 
 
 def main():
