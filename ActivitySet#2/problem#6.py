cbd55c31d765dcf138513cd56653b1ad5a232715
Hitaishkg <hitaishkg@gmail.com>
2022-06-29 10:27:42 +0530

    ActivitySet_2p-6 done

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,11 +1,22 @@
-
-
-class Menu:
+class Menu(dict):
     """fill in class definition here"""
+    def __init__(self) -> None:
+        self=dict()
+
+    def add(self,item,price):
+        self[item]=price
+
+    def show(self):
+        l = ' '
+        for i in self:
+            l += i+" "+str(self.get(i))
+            x=l[:8]
+            y=l[8:]
+        print(x)
+        print(y)
 
 
 m = Menu()  # Menu is a class
-m.add("idly", 10)
-m.add("vada", 20)
-
+m.add('idly', 10)
+m.add('vada', 20)
 m.show()
