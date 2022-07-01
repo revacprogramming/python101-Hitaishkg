8a454f8671f1402d9a82119ac7d1b2381e6d14fe
Hitaishkg <hitaishkg@gmail.com>
2022-07-01 23:42:55 +0530

    p7 in progress

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -2,6 +2,13 @@
 
 class Menu:
     """fill in class definition here"""
+    def __add__(self,item,price):
+        self.item=item
+        self.price=price
+        return self.item+self.price
+
+    
+
 
 
 m = Menu()
