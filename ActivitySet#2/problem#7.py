612f4484e14cca9646cdcccc9d89f3d06f8fa639
Hitaishkg <hitaishkg@gmail.com>
2022-07-03 18:53:52 +0530

    ActivitySet2 p7 done

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,17 +1,17 @@
-
-
 class Menu:
-    """fill in class definition here"""
-    def __add__(self,item,price):
-        self.item=item
-        self.price=price
-        return self.item+self.price
-
-    
-
-
-
+    def __init__(self):
+      self.foods={}
+    def __add__(self,x):
+      if(x[0] in self.foods):
+        self.foods[x[0]]+=x[1]
+      else:
+        self.foods[x[0]]=x[1]
+      return self
+    def __str__(self):
+      k=""
+      for a in self.foods:
+        k+=(f"{a}  {self.foods[a]}\n")
+      return(k[:-1])
 m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
-
-print(m)  # should print the menu properly
+m = m + ("idly", 10)+ ("vada",20) 
+print(m)
