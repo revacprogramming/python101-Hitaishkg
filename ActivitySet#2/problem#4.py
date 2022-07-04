43f28361f455df4b2e9f8a4620e27da20205c7d3
Hitaishkg <hitaishkg@gmail.com>
2022-07-04 14:12:07 +0530

    activitySet2 p4 updated(using list comprihension)

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,30 +1,4 @@
 
-
-# def get_cs():
-#     """get string input"""
-  
-
-
-# def cs_to_lot(cs):
-#     """convert connected string to list of strings"""
-
-
-# def lot_to_cs(lot):
-#     """convert list of strings to connected string"""
-
-
-# def main():
-#     cs=get_cs()
-
-#     lot=cs_to_lot(cs)  # convert connect string to list of tuples
-#     print(lot)
-
-#     cs=lot_to_cs(lot)  # convert list of strings to connect string
-#     print(cs)
-
-
-# if __name__ == '__main__':
-#     main()
 def get_cs():
     s = input("Enter string: ")
     return s
@@ -32,11 +6,12 @@ def get_cs():
 
 def cs_to_lot(cs):
     """convert connected string to list of strings"""
-    l = []
+    l=[]
     cs = cs.split(";")
-    for i in cs:
-        i = tuple(i.split("="))
-        l.append(i)
+    l=[tuple(i.split('=')) for i  in cs]
+    # for i in cs:
+    #     i = tuple(i.split("="))
+    #     l.append(i)
     return l
 
 
